3f3065b3b869637f8ccb6acb5ff01c4458ac7cb8
Duy <hoangduy2812002@gmail.com>
2022-12-16 14:33:52 +0700

    commit

--- Server.js
+++ Server.js
@@ -16,31 +16,71 @@ server.listen(3002, () => {
   console.log("Server is running");
 });
 
+var listObject = {};
+var listSocket = [];
 
-io.on("connection", function (socket) {
-  console.log("co nguoi vua ket noi",socket.id)
-  socket.on("join_room",(data)=>{
+io.on("connection", async function (socket) {
+  console.log("co nguoi vua ket noi", socket.id);
+
+  socket.on("connectUser", (email) => {
+
+    listObject = {};
+    listObject.socketId = socket.id;
+    listObject.email = email;
+    listSocket.push(listObject);
+    io.sockets.emit("server-send-listSocket", listSocket);
+  });
+  // console.log("END----->>", listSocket);
+
+  socket.on("join_room", (data) => {
     socket.join(data);
-    console.log("User joined room: "+data)
+    console.log("User joined room: " + data);
   });
 
-  socket.on("send_message",(data)=>{
-    console.log("data-->",data)
-    socket.to(data.room).emit("recevie_message",data.content);
-    // socket.to(data.room).emit("get_one_message");
-    socket.broadcast.emit("get_one_message");
-  })
+  socket.on("send_message", (data) => {
+    console.log("data-->", data);
 
-  socket.on("I'm typing",(room,data)=>{
-    // console.log("data",data)
-    socket.broadcast.emit(room+"user-typing",data.studentCode,data.fullName+" đang soạn tin nhắn...")
-  })
-  socket.on("I stopped typing",(room)=>{
-    socket.broadcast.emit(room+"stop-user-typing","")
-  })
+    socket.to(data.room).emit("recevie_message", data.content);
+    // socket.broadcast.emit("get_one_message");
+    io.sockets.emit("get_one_message");
+  });
+
+  socket.on("create-group", function () {
+    console.log("create-group")
+    io.sockets.emit("get_one_message");
+  });
+  socket.on("add-friend", function () {
+    console.log("add-friend")
+    socket.broadcast.emit("request-accept");
+  });
+
+  socket.on("accept-friend-request", function () {
+    console.log("accept-friend-request")
+    io.sockets.emit("accept");
+  });
+  
+
+  socket.on("I'm typing", (room, data) => {
+    console.log("data", data);
+    socket.broadcast.emit(
+      room + "user-typing",
+      data,
+      data.fullName + " đang soạn tin nhắn..."
+    );
+  });
+  socket.on("I stopped typing", (room) => {
+    socket.broadcast.emit(room + "stop-user-typing", "");
+  });
+
+  socket.on("isSeen", (room) => {
+    io.sockets.emit("seen");
+  });
 
   socket.on("disconnect", function () {
+    listSocket.splice(listSocket.indexOf(socket.id), 1);
     console.log(socket.id + " ngat ket noi!!!!!!");
+    io.sockets.emit("server-send-listSocket", listSocket);
+    console.log("listSocket disconnect----->>", listSocket);
   });
 
   socket.on("Client-request-like", function () {
@@ -55,25 +95,9 @@ io.on("connection", function (socket) {
     io.sockets.emit("Server-response-like-comment");
   });
 
-  // socket.on("client-user-connect", function (customer, account) {
-  //   // console.log("customer----->>", customer);
-  //   // console.log("account----->>", account);
-  //   io.sockets.emit(customer.studentCode,account);
-  //   io.sockets.emit(account.studentCode,customer);
-  // });
-
-  // socket.on("client-send-message", function (customer, account,content) {
-  //   console.log("from",customer)
-  //   console.log("to",account)
-  //   console.log("content",content)
-  //   io.sockets.emit('server-send'+customer.studentCode,content);
-  //   io.sockets.emit('server-send'+account.studentCode,content);
-
-  //   // io.sockets.emit("Server-response-like-comment");
-  // });
-
-  // socket.on("logout", function () {
-  //   mangUsers.slice(mangUsers.indexOf(socket.studentCode), 1);
-  //   io.broadcast.emit("server-send-danhSach-Users", mangUsers);
-  // });
+  socket.on("logout", function (account) {
+    listSocket.splice(listSocket.indexOf(socket.id), 1);
+    io.sockets.emit("server-send-listSocket", listSocket);
+    console.log("listSocket----->>", listSocket);
+  });
 });
